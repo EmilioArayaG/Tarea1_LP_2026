a31b95f04c40d859c8038f611fd294f23b07b87e
emilio.araya <emiliosaraya@gmail.com>
2026-03-11 23:49:31 -0300

    Commit miercoles, arreglar procesar archivo, hacer de nuevo entenderlo

--- Evaluador.py
+++ Evaluador.py
@@ -18,8 +18,58 @@ camelCase = rf"{letra_min}+(?:{letra_may}+{letra_min}+)+"
 PascalCase = rf"(?:{letra_may}+{letra_min}*)+"
 nombre_valido = rf"(?:{snake_case}|{camelCase}|{PascalCase})"
 
-condicion = rf"(?:\(\s*{nombre_valido}\s*{operacion}\s*{valor}\))"
-estructura_control = rf"(?:(?:if|while)\s*{condicion}\s*\{{)"
+condicion = rf"(?:\(\s*{nombre_valido}\s*{operacion}\s*{valor}\s*\))"
+estructura_control = rf"(?:(?:if|while)\s+{condicion}\s*\{{)"
 cierre_bloque = r"\}"
-declaracion_funcion = rf"(?:{tipo_dato}\s+{nombre_valido}\s*\(\s*\)\{{)"
+detector_funcion = rf"(?:{tipo_dato})\s+({palabra})\s*\("
 declaracion_variable = rf"(?:{tipo_dato}\s+{nombre_valido}\s*{operacion}\s*{valor}\s*;)"
+retorno = rf"(?:\s*return\s+{nombre_valido}\s*;)"
+comentario_one_line = rf"//\s*{palabra}"
+comentario_multi_line = rf"/\*\s*{palabra}\s*\*/"
+
+def determinar_autor(nombre_funcion):
+    '''
+    ***
+    Parametro: nombre_funcion: str que contiene el nombre de la funcion
+    ***
+    Retorno: str
+    ***
+    Evalua el nombre de la funcion y retorna al que pertenece
+    '''
+    if re.fullmatch(snake_case, nombre_funcion):
+        return "snake"
+    elif re.fullmatch(camelCase, nombre_funcion):
+        return "camel"
+    elif re.fullmatch(PascalCase, nombre_funcion):
+        return "Pascal"
+    else:
+        return "desconocido"
+    
+def procesar_archivo():
+    '''
+    ***
+    Parametro: None
+    ***
+    Retorno: None
+    ***
+    Esta funcion lee el archivo programa.txt, va leyendo cada linea y evalua el contexto
+    correspondiente
+    '''
+    contexto_actual = None
+
+    with open('programa.txt','r',encoding='utf-8') as archivo:
+        for linea in archivo:
+            linea_limpia = linea.strip()
+
+            if not linea_limpia:
+                continue
+
+            match_funcion = re.search(detector_funcion, linea_limpia)
+            if match_funcion:
+                nombre = match_funcion.group(1)
+                contexto_actual=determinar_autor(nombre)
+
+                print((f"linea: {linea_limpia}"))
+                print(f"Funcion detectada Nombre: {nombre}| autor: {contexto_actual}")
+
+procesar_archivo()
